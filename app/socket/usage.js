1742b96785b682693d6d18b32d58d9d4e415dbd3
Pearce <pearce.ropion@me.com>
2019-04-03 23:06:49 -0700

    Added sockets

---
+++ app/socket/usage.js
@@ -0,0 +1,32 @@
+import uuidv4 from 'uuid/v4';
+import { get } from 'lodash';
+
+import IO from '../socket';
+import app from '../app';
+import { getUsage } from '../api/usages';
+
+IO.connection('connection', socket => {
+    const usageID = uuidv4();
+    IO.emit('usageID', { usageID });
+
+    socket.on('request', (from, data) => {
+        const requestID = get(data, 'requestID', null);
+        if (!requestID || requestID !== usageID) {
+            return;
+        }
+
+        const variables = get(data, 'variables', {});
+        const include = get(data, 'include', []);
+
+        getUsage(app.locals.db, { variables, include })
+            .then(result => {
+                IO.emit('usage', { data: result });
+            }).catch(error => {
+                IO.emit('usage', { error });
+            });
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnect');
+    });
+});
