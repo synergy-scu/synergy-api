4e5463cddaf275595e3a8c17c601a4e28649b7f3
Pearce <pearce.ropion@me.com>
2019-04-06 15:05:05 -0700

    Added time to usage emit interval

--- app/socket/usage.js
+++ app/socket/usage.js
@@ -5,8 +5,19 @@ import IO from '../socket';
 import app from '../app';
 import { getUsage } from '../api/usages';
 
+export const getLatestUsage = (variables, include, refreshRate) =>
+    setInterval(() => {
+        getUsage(app.locals.db, { variables, include })
+            .then(result => {
+                IO.emit('usage', { data: result });
+            }).catch(error => {
+                IO.emit('usage', { error });
+            });
+    }, refreshRate);
+
 IO.connection('connection', socket => {
     const usageID = uuidv4();
+    let usageLoopId = null;
     IO.emit('usageID', { usageID });
 
     socket.on('request', (from, data) => {
@@ -15,18 +26,15 @@ IO.connection('connection', socket => {
             return;
         }
 
+        const refreshRate = get(data, 'refreshRate', 750);
         const variables = get(data, 'variables', {});
         const include = get(data, 'include', []);
 
-        getUsage(app.locals.db, { variables, include })
-            .then(result => {
-                IO.emit('usage', { data: result });
-            }).catch(error => {
-                IO.emit('usage', { error });
-            });
+        usageLoopId = getLatestUsage(variables, include, refreshRate);
     });
 
     socket.on('disconnect', () => {
+        clearInterval(usageLoopId);
         console.log('Client disconnect');
     });
 });
